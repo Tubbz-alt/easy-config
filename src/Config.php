5a3fda6d780ca9e8710a206463670a3c367f4e65
tom.koukoulis <tom.koukoulis@shazam.com>
2014-08-14 11:47:46 +0100

    ITL-502: Prepare for open source

--- src/Config.php
+++ src/Config.php
@@ -13,6 +13,7 @@
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
  * language governing permissions and limitations under the License.
  *
+ * @author Tom Koukoulis <tom.koukoulis@shazam.com>
  * @author toni lopez <toni.lopez@shazam.com>
  * @package Common
  */
